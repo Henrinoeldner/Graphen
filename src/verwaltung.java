9c8a23f6852b685846f32a89e13ee7205e3c9ce5
noeldnhe <henri.noeldner@frg.wwschool.de>
2025-04-11 10:29:39 +0200

    Test

---
+++ src/verwaltung.java
@@ -0,0 +1,16 @@
+import untils.*;
+import untils.graph.*;
+
+public class verwaltung {
+    public static void main(String[] args) {
+        new verwaltung();
+        System.out.println("Herzlich willkommen zu Zeichenketten verschluesselerer!");
+    }
+    public verwaltung(){
+        Graph schulgraph=   new Graph();
+        schulgraph.addVertex(new Vertex("Inforaum -1.030"));
+        schulgraph.addVertex(new Vertex("Aula"));
+        schulgraph.getVertex(new Vertex());
+        schulgraph.addEdge(new Edge(schulgraph.getVertex("Inforaum -1.030"),schulgraph.getVertex("Aula"),18));
+    }
+}
